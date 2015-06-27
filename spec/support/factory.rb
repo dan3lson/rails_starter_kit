b0e2ab0e40ff69fb40663c9a497c77dbf35b54e5
Danelson Rosa <danelson.rosa@gmail.com>
2015-06-27 09:10:07 -0400

    Updated FactoryGirl files and database.yml replacement code.

---
+++ spec/support/factory.rb
@@ -0,0 +1,16 @@
+FactoryGirl.define do
+  # Below are example factories that can be used
+  # in your unit and acceptance tests.
+  
+  # factory :person do
+  #   fname "John"
+  #   lname "Doe"
+  # end
+  #
+  # factory :car do
+  #   color "black"
+  #   type "S"
+  #   maker "Tesla"
+  #   person
+  # end
+end
